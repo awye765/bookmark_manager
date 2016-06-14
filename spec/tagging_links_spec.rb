7aaf9b7b243eb77b68603c90a203ff6e29c97953
awye765 <awye765@googlemail.com>
2016-06-14 17:55:31 +0100

    Added multiple tagging functionality for links

--- spec/tagging_links_spec.rb
+++ spec/tagging_links_spec.rb
@@ -7,4 +7,15 @@ feature "Tagging links" do
     link = Link.first
     expect(link.tags.map(&:name)).to include("code")
   end
+
+  scenario "can add multiple tags to a new link" do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Submit'
+    link = Link.first_or_create
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+
 end
